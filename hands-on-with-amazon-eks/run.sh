64181d6e2bc4f623e65f72b27428233360ae79a7
Marcos Dutra <macdutra@MacBook-Pro-de-Marcos.local>
2025-09-03 13:53:41 -0300

    update files

--- hands-on-with-amazon-eks/run.sh
+++ hands-on-with-amazon-eks/run.sh
@@ -18,3 +18,40 @@ echo "Deploy ssl app..."
 echo "Install external dns..."
 cd ../../external-dns
 ./create.sh
+
+echo "Creating dynamodb tables..."
+cd ../../../clients-api/infra/cloudformation
+echo "clients-api table..."
+./create-dynamodb-table.sh development
+cd ../../../inventory-api/infra/cloudformation
+echo "inventory-api table..."
+./create-dynamodb-table.sh development
+cd ../../../renting-api/infra/cloudformation
+echo "renting-api table..."
+./create-dynamodb-table.sh development
+cd ../../../resource-api/infra/cloudformation
+echo "resource-api table..."
+./create-dynamodb-table.sh development
+
+echo "Add dynamodb permission to nodes..."
+cd ../../../marcos_scripts
+./dynamodb_permission.sh
+
+echo "Installing helm..."
+cd ../clients-api/infra/helm
+echo "clients-api helm..."
+./create.sh
+cd ../../../inventory-api/infra/helm
+echo "inventory-api helm..."
+./create.sh
+cd ../../../renting-api/infra/helm
+echo "renting-api helm..."
+./create.sh
+cd ../../../resource-api/infra/helm
+echo "resource-api helm..."
+./create.sh
+cd ../../../front-end/infra/helm
+echo "front-end helm..."
+./create.sh
+
+
