1a8efcacf7fbf3bd87d1a6efd6cbce53d90ee53a
Marcos Dutra <macdutra@MacBook-Pro-de-Marcos.local>
2025-09-03 10:04:27 -0300

    Add run.sh

---
+++ hands-on-with-amazon-eks/run.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo "Creating the cluster..."
+cd Infrastructure/eksctl/01-initial-cluster
+eksctl create cluster -f cluster.yaml
+
+echo "Configuring the load balancer..."
+cd ../../k8s-tooling/load-balancer-controller
+./create.sh
+
+echo "Deploy sample app..."
+cd test
+./run.sh
+
+echo "Deploy ssl app..."
+./run-with-ssl.sh
+
+echo "Install external dns..."
+cd ../../external-dns
+./run.sh
